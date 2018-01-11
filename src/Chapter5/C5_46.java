bc17d385f6076793a7b69f9b110494c7433a9eef
AaronForehand <af0966989@RK03-B1225-06.francistuttle.edu>
2018-01-11 09:08:15 -0600

    Second version of Java Programming-Added Chapters 5 and 6

---
+++ src/Chapter5/C5_46.java
@@ -0,0 +1,24 @@
+package Chapter5;
+import java.util.Scanner;
+/**
+ * Program takes a given string from user and reverses it, then prints it to console
+ * 
+ * @author Aaron Forehand
+ */
+public class C5_46 {
+        /**
+     * Main Method
+     *
+     * @param args arguments from command line prompt
+     */
+
+    	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter string for reversal: ");
+		String reverse = input.next();
+		
+		System.out.printf("%s reversed is %s.", reverse, (new StringBuffer(reverse).reverse().toString()));
+	}
+
+}
---
+++ src/Chapter5/P5.java
@@ -0,0 +1,45 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+/**
+ * Program to take votes and count them up, displaying final tallies to the
+ * console
+ *
+ * @author Aaron Forehand
+ */
+public class P5 {
+
+    /**
+     * Main Method
+     *
+     * @param args arguments from command line prompt
+     */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char vote;
+        int yesVotes = 0;
+        int noVotes = 0;
+        int finished = 0;
+        String voteString;
+        while (finished != 1) {
+            System.out.println("Enter 'Y' to vote yes, 'N' to vote no, or 'Q' to quit voting. ");
+            voteString = input.nextLine();
+
+            vote = voteString.charAt(0);
+
+            if ((vote == 'Y') || (vote == 'y')) {
+                yesVotes++;
+            } else if ((vote == 'N') || (vote == 'n')) {
+                noVotes++;
+            } else if ((vote == 'Q') || (vote == 'q')) {
+                System.out.printf("Total Votes: %d voted yes, and %d voted no. ", yesVotes, noVotes);
+                finished = 1;
+            } else {
+                System.out.println("INVALID CODE: Enter 'Y' to vote yes, 'N' to vote no, or 'Q' to quit voting. ");
+            }
+
+        }
+    }
+}
